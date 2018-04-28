df735743d16352a4d17cfb3f3550f2bee807511c
James McNeil <jmcneil@equalexperts.com>
2018-04-28 09:54:31 +0100

    week 3: uses median of three as pivot

--- algorithms-1/week-3/quicksort.py
+++ algorithms-1/week-3/quicksort.py
@@ -1,3 +1,5 @@
+import math
+
 def quicksort(array):
     sorted_array = array
     comparisons = recursively_partition(sorted_array, 0, len(array) - 1)
@@ -30,17 +32,35 @@ def recursively_partition(array, left, right):
 #     return array[left]
 
 
-#Using the right most index as the pivot
+# Using the right most index as the pivot
+# def choose_pivot(array, left, right):
+#     swap_pivot(array, left, right)
+#     return array[left]
+
+
+# Using the median of three as the pivot
 def choose_pivot(array, left, right):
-    pivot = array[right]
-    array[right] = array[left]
+    middle = left + int(math.ceil((right - left - 1)/2))
+    if is_median(array, middle, left, right):
+        swap_pivot(array, left, middle)
+    if is_median(array, right, middle, left):
+        swap_pivot(array, left, right)
+    return array[left]
+
+
+def is_median(array, test, a, b):
+    return (array[a] <= array[test] <= array[b]) or (array[b] <= array[test] <= array[a])
+
+
+def swap_pivot(array, left, index):
+    pivot = array[index]
+    array[index] = array[left]
     array[left] = pivot
-    return pivot
 
 
 def run():
     with open('./quicksort-data.txt') as f:
-        data = f.readlines()
+        data = ([int(line) for line in f])
         quicksort(data)
 
 
